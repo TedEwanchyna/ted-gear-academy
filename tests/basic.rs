54ca10161459cf13b2e54880ad8929787f5daec2
TedEwanchyna <ted.ewanchyna@gmail.com>
2024-05-08 00:29:33 -0400

    Update basic.rs

--- tests/basic.rs
+++ tests/basic.rs
@@ -1,15 +1,15 @@
-use pebbles_game_io::*;
-use gstd::{*};
-use gtest::{Log, Program, System};
 use crate::scale_info::prelude::time::SystemTime;
 use crate::scale_info::prelude::time::UNIX_EPOCH;
-
+use gstd::*;
+use gtest::{Log, Program, System};
+use pebbles_game_io::*;
+/*
 const ADMIN: u64 = 100;
 const MAX_NUMBER_OF_TURNS: u32 = 21; // for loop counter
 const MAX_PEBBLES_PER_TURN: u32 = 2;
 const PEBBLES_COUNT: u32 = 15;
 const DIFFICULTY: DifficultyLevel = DifficultyLevel::Easy;
-
+*/
 /// test
 /// test
 /// test
